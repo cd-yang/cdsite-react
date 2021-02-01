51681c77ce322893281e15d91fc42a40908f9cc8
cdyang <nwpu409@gmail.com>
2021-02-01 08:18:40 +0800

    change about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,31 @@
 export default function About() {
     return (
-        <div className="about">
-            <h1>I am a mysterious person!</h1>
-            <p>I'm only available in the internet</p>
+        // style="margin-top:10px;"
+        <div className="container">
+            <h3>Me and the Blog</h3>
+            <hr />
+            <div className="row">
+                <div className="col-md-7">
+                    <section id="self-intro">
+                        <blockquote className="blockquote">
+                            <p>
+                                此博客为学习 .Net Core 和 Vue.js 学习的作品。
+                        </p>
+                        </blockquote>
+                        <p>
+                            Contact me: <a href="mailto:yang.caidong@foxmail.com">yang.caidong@foxmail.com</a>
+                        </p>
+                        <div>
+                            <a href="https://github.com/cd-yang" className="btn btn-outline-success" target="_blank">
+                                <span className="icon-github"></span>
+                            GitHub
+                        </a>
+
+                        </div>
+                    </section>
+                </div>
+
+            </div>
         </div>
     );
 }
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,6 @@
 import { Route, Switch } from 'react-router';
 import About from './About';
 import Blog from './Blog';
-import Contact from './Contact';
 import Post from './Post';
 
 export default function Body() {
@@ -9,7 +8,6 @@ export default function Body() {
         <Switch>
             <Route path="/" exact component={Blog} />
             <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
             <Route path="/post/:slug" component={Post} />
         </Switch>
     );
