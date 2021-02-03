09d644d227dad98ed3856aa7f5b4c15892a2d2d4
cdyang <nwpu409@gmail.com>
2021-02-03 22:27:49 +0800

    change about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,31 +1,28 @@
 export default function About() {
     return (
         <div className="container">
-            <h3>Me and the Blog</h3>
+            <h2>
+                前端学习中 · 骑行爱好者
+            </h2>
             <hr />
+            <h3>
+                Powered by
+                <a target="_blank" rel="noreferrer" href="https://reactjs.org/" style={{ margin: "0px 4px" }}>
+                    <img alt="React" src="https://img.shields.io/badge/-React-77dffc?logo=react&logoColor=ffffff"></img>
+                </a>
+                and
+                    <a target="_blank" rel="noreferrer" href="https://dotnet.microsoft.com/" style={{ marginLeft: "4px" }} >
+                    <img alt=".Net 5" src="https://img.shields.io/badge/.NET5-6e16e8?logo=C-Sharp&logoColor=ffffff" />
+                </a>
+            </h3>
             <div className="row">
                 <div className="col-md-7">
                     <section id="self-intro">
-                        <blockquote className="blockquote">
-                            <p>
-                                Powered by
-                                <a target="_blank" rel="noreferrer" href="https://reactjs.org/" style={{ margin: "0px 4px" }}>
-                                    <img alt="React" src="https://img.shields.io/badge/-React-77dffc?logo=react&logoColor=ffffff"></img>
-                                </a>
-                                and
-                                <a target="_blank" rel="noreferrer" href="https://dotnet.microsoft.com/" style={{ marginLeft: "4px" }} >
-                                    <img alt=".Net 5" src="https://img.shields.io/badge/.NET5-6e16e8?logo=C-Sharp&logoColor=ffffff" />
-                                </a>
-                            </p>
-                        </blockquote>
                         <p>
                             Contact me: <a href="mailto:yang.caidong@foxmail.com">yang.caidong@foxmail.com</a>
                         </p>
                         <div>
-                            <a href="https://github.com/cd-yang" target="_blank" rel="noreferrer">
-                                <span className="icon-github"></span>
-                                GitHub
-                            </a>
+                            <a href="https://github.com/cd-yang" target="_blank" rel="noreferrer">GitHub</a>
                         </div>
                     </section>
                 </div>
